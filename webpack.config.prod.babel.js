992017acd11837c65364f3bbf32013f5c0c210b4
David Henning <david.henning@inovex.de>
2017-01-08 20:29:20 +0100

    Add Webpack config files for development and production

---
+++ webpack.config.prod.babel.js
@@ -0,0 +1,85 @@
+import webpack from 'webpack'
+import path from 'path'
+import autoprefixer from 'autoprefixer'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+
+let extractStyles = new ExtractTextPlugin('[name].css')
+let extractHtml = new ExtractTextPlugin('[name].html')
+
+let config = {
+  stats: {
+    assets: false,
+    colors: true,
+    version: false,
+    hash: true,
+    timings: true,
+    chunks: false,
+    chunkModules: false
+  },
+  entry: {
+    index: [
+      path.resolve(__dirname, 'templates/index.pug')
+    ],
+    post: [
+      path.resolve(__dirname, 'templates/post.pug')
+    ],    
+    'css/application': [
+      path.resolve(__dirname, 'assets/styles/application.scss')
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        loader: extractHtml.extract({
+          loader: ['html-loader', 'pug-html-loader?pretty&exports=false']
+        })
+      },
+      {
+        test: /\.scss$/,
+        loader: extractStyles.extract({
+          loader: [
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'csso-loader?-restructure'
+            },            
+            {
+              loader: 'postcss-loader'
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: false,
+      debug: true,
+      options: {
+        postcss: [
+          autoprefixer({
+            browsers: ['last 2 version', 'Explorer >= 10', 'Android >= 4']
+          })
+        ],
+        sassLoader: {
+          includePaths: [
+            path.resolve(__dirname, 'node_modules/sanitize.css/')
+          ]
+        }
+      }
+    }),
+    extractStyles,
+    extractHtml
+  ]
+}
+
+export default config
