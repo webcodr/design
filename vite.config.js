db3d4da13eea344ed64657e10c76305798c62cd5
David Henning <david@webcodr.io>
2024-10-22 20:34:32 +0200

    Remove unused dependency

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,15 @@ export default defineConfig({
     build: {
         rollupOptions: {
             output: {
-                manualChunks: undefined,
+                assetFileNames: (assetInfo) => {
+                    // Check the extension of the asset, adjust the regular expression as needed
+                    if (/\.(ttf|otf|eot|woff|woff2)$/.test(assetInfo.name)) {
+                        // Prevent renaming by returning the original file name
+                        return 'fonts/[name][extname]';
+                    }
+                    // Default behavior for other assets
+                    return 'assets/[name].[hash][extname]';
+                },
             },
         },
     },
