21ab705990fde36fb49fb03955e7d9d1b3ee3d98
David Henning <david@webcodr.io>
2024-10-24 19:07:08 +0200

    Replace Pug templates with native HTML

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
-import vitePugPlugin from 'vite-plugin-pug-transformer';
 
 export default defineConfig({
-    plugins: [vitePugPlugin()],
     css: {
         preprocessorOptions: {
             scss: {
