bd28d4938018fba91920ac59bdb860c3800ee9a5
David <david@webcodr.io>
2025-06-04 18:40:08 +0200

    Update design

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,21 +1,5 @@
-import {
-  getPreferredTheme,
-  setPreferredTheme,
-  togglePreferredTheme
-} from "./theme.js"
-
 import { setLanguage } from "./code-language.js"
 
-setPreferredTheme(getPreferredTheme())
-
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("#toggle-checkbox").checked =
-    getPreferredTheme() === "dark"
-  document
-    .querySelector("#theme-toggle")
-    .addEventListener("click", togglePreferredTheme)
-  document
-    .querySelector("#toggle-checkbox")
-    .addEventListener("click", togglePreferredTheme)
   setLanguage()
 })
